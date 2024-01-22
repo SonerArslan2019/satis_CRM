3846a6f2829261037640c81e59150d054e78e11b
SonerArslan2019 <soner@arslanyapi.com.tr>
2024-01-22 09:57:37 +0300

    ana yapi olusturuluyor5

--- satis_CRM/settings.py
+++ satis_CRM/settings.py
@@ -27,7 +27,12 @@ INSTALLED_APPS = [
     'user',
     'satis',
     # 3rd
+    "crispy_forms",
+    "crispy_bootstrap5",
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
